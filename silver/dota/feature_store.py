52dbfc6f992bcee59e633481b00a6b495d980c9c
teocalvo2@gmail.com <teocalvo2@gmail.com>
2023-01-11 00:14:55 +0000

    Feature Store Player

---
+++ silver/dota/feature_store.py
@@ -0,0 +1,69 @@
+# Databricks notebook source
+# DBTITLE 1,Imports
+import datetime
+
+from databricks import feature_store
+
+# COMMAND ----------
+
+# DBTITLE 1,Funcoes
+def import_query(path:str)->str:
+    with open(path, "r") as open_file:
+        query = open_file.read()
+    return query
+
+def table_exists(schema:str, table:str)->bool:
+    count = spark.sql(f"SHOW TABLES FROM {schema}").filter(f"tableName = '{table}'").count()
+    return count > 0
+
+def range_date(date_start:str, date_stop:str)->[str]:
+    
+    dt_start = datetime.datetime.strptime(date_start, "%Y-%m-%d")
+    dt_stop = datetime.datetime.strptime(date_stop, "%Y-%m-%d")
+    dates = []
+    
+    while dt_start <= dt_stop:
+        dates.append(dt_start.strftime("%Y-%m-%d"))
+        dt_start += datetime.timedelta(days=1)
+        
+    return dates
+
+# COMMAND ----------
+
+# DBTITLE 1,Setup
+schema = 'silver_gamelakehouse'
+fs_name = 'fs_player_summary'
+table_name = f'{schema}.{table}'
+dt_start = '2022-01-01'
+dt_stop = '2023-01-11'
+lag = 150
+
+fs_name += f"_lag{lag}"
+
+# COMMAND ----------
+
+# DBTITLE 1,Processo
+fs_client = feature_store.FeatureStoreClient()
+dates = range_date(dt_start, dt_stop)
+query = import_query(f"sql/{fs_name}.sql")
+
+if not table_exists(schema, fs_name):
+    query_exec = query.format(date=date.pop(0), lag=lag)
+    
+    df = spark.sql(query_exec)
+    
+    fs_client.create_table(name=table_name, 
+                           primary_keys=['dtReference','idAccount'],
+                           df=df,
+                           partition_columns= ['dtReference'])
+
+for i in dates:
+    query_exec = query.format(date=i, lag=lag)
+    df = spark.sql(query_exec)
+    fs_client.write_table(name=table_name, df=df, mode='merge')
+    print(i, "- Done!")
+    
+
+# COMMAND ----------
+
+
---
+++ silver/dota/sql/fs_player_summary.sql
@@ -0,0 +1,56 @@
+WITH tb_summary AS (
+
+  SELECT account_id AS idAccount,
+         MIN(DATEDIFF('{date}', dt_match)) AS qtRecency,
+         COUNT(DISTINCT match_id) AS qtMatches,
+         AVG(assists) AS avgAssists,
+         AVG(camps_stacked) AS avgCampsStacked,
+         AVG(creeps_stacked) AS avgCreepsStacked,
+         AVG(deaths) AS avgDeaths,
+         AVG(denies) AS avgDenies,
+         AVG(firstblood_claimed) AS avgFirstbloodClaimed,
+         AVG(gold) AS avgGold,
+         AVG(gold_per_min) AS avgGoldPerMin,
+         AVG(gold_spent) AS avgGoldSpent,
+         AVG(hero_damage) AS avgHeroDamage,
+         AVG(hero_healing) AS avgHeroHealing,
+         AVG(kills) AS avgKills,
+         AVG(last_hits) AS avgLastHits,
+         AVG(level) AS avgLevel,
+         AVG(net_worth) AS avgNetWorth,
+         AVG(roshans_killed) AS avgRoshansKilled,
+         AVG(rune_pickups) AS avgRunePickups,
+         AVG(xp_per_min) AS avgXpPerMin,
+         AVG(win) AS avgWin,
+         AVG(total_gold) AS avgTotalGold,
+         AVG(total_xp) AS avgTotalXp,
+         AVG(kills_per_min) AS avgKillsPerMin,
+         AVG(kda) AS avgKda,
+         AVG(neutral_kills) AS avgNeutralKills,
+         AVG(tower_kills) AS avgTowerKills,
+         AVG(courier_kills) AS avgCourierKills,
+         AVG(lane_kills) AS avgLaneKills,
+         AVG(hero_kills) AS avgHeroKills,
+         AVG(observer_kills) AS avgObserverKills,
+         AVG(sentry_kills) AS avgSentryKills,
+         AVG(roshan_kills) AS avgRoshanKills,
+         AVG(necronomicon_kills) AS avgNecronomiconKills,
+         AVG(ancient_kills) AS avgAncientKills,
+         AVG(buyback_count) AS avgBuybackCount,
+         AVG(observer_uses) AS avgObserverUses,
+         AVG(sentry_uses) AS avgSentryUses,
+         AVG(lane_efficiency) AS avgLaneEfficiency
+
+  FROM bronze_gamelakehouse.dota_match_players
+
+  WHERE dt_match < '{date}'
+  AND dt_match >= date_add('{date}', -{lag})
+
+  GROUP BY account_id
+)
+
+SELECT 
+      DATE('{date}') AS dtReference,
+      *
+
+FROM tb_summary
