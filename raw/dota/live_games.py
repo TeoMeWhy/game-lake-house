9166b0e7120716dfcf6281ae691b29092bb47f80
Téo Calvo <teocalvo2@gmail.com>
2023-02-20 19:01:17 -0300

    Auto ingestion for live games in raw

--- raw/dota/live_games.py
+++ raw/dota/live_games.py
@@ -1,5 +1,9 @@
+import argparse
 import boto3
+import datetime
+import dotenv
 import json
+import os
 import requests
 import schedule
 import time
@@ -14,8 +18,12 @@ def get_data(**kwargs):
 def filter_data(data):
     new_data = []
     for d in data:
-        if d["league_id"] != 0:
+        if d == None:
+            continue
+
+        elif d["league_id"] != 0:
             new_data.append(d)
+
     return new_data
 
 
@@ -30,14 +38,30 @@ def save_data_firehose(data, firehose):
     )
 
 
-def execute(firehose):
-    data = filter_data(get_data())
-    save_data_firehose(data, firehose)
+def execute(firehose, api_key):
+    print(datetime.datetime.now())
+    data = get_data(api_key=api_key)
+    filtered_data = filter_data(data)
+    save_data_firehose(filtered_data, firehose)
 
 
 def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--interval",
+        "-i",
+        help="Intervalo em minutos para execução",
+        default=5,
+        type=int,
+    )
+    args = parser.parse_args()
+
+    dotenv.load_dotenv(".env")
+    api_key = os.getenv("API_KEY")
+
     firehose = boto3.client("firehose", region_name="us-east-1")
-    schedule.every(1).minutes.do(execute, firehose)
+    schedule.every(args.interval).minutes.do(execute, firehose, api_key)
 
     while True:
         schedule.run_pending()
